9be226bf5021434c7a5d09d2782e536f4033bc12
Yatsko Ivan <ivanyatsko@mail.ru>
2021-04-12 23:40:54 +0300

    feat: solution task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,27 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  if (!str) {
+    return ('');
+  }
+  const array = str.split('');
+  let newString = '';
+  let sum = 0;
+  let i = array[0];
+  array.forEach((item, k) => {
+    if (i === item) {
+      sum++;
+    } else {
+      newString += (sum === 1) ? i : sum + i;
+      sum = 1;
+      i = item;
+    }
+    if (k === array.length - 1) {
+      newString += (sum === 1) ? i : sum + i;
+    }
+  });
+  return newString;
 }
 
 module.exports = encodeLine;
